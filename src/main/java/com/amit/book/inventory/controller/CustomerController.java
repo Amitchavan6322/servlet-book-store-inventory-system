fd7386ade97d38d903b27060f94d775fe2600949
amitchavan6322 <amitchavan6322@gmail.com>
2025-04-05 20:15:45 +0530

    added changes to html code

--- src/main/java/com/amit/book/inventory/controller/CustomerController.java
+++ src/main/java/com/amit/book/inventory/controller/CustomerController.java
@@ -15,7 +15,6 @@ public class CustomerController extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         System.out.println("@@@@@@@@ inside the CustomerController doGet() method");
-        try {
             int id = Integer.parseInt(request.getParameter("customerid"));
             String name = request.getParameter("customername");
             String address = request.getParameter("customeraddress");
@@ -28,11 +27,6 @@ public class CustomerController extends HttpServlet {
             writer.println("<html><body>");
             writer.println(customerService.getCustomerDetails(customer));
             writer.println("</body></html>");
-        } catch (Exception e) {
-            // Redirect to the form page again if there's an error
-            System.out.println("Error occurred: " + e.getMessage());
-            response.sendRedirect(request.getContextPath() + "/customer-insert.html");
-        }
     }
 
     public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
